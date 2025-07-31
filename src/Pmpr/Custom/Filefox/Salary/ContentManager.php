af9991c106884d0bd383f9b5328e5f4ce6973f38
pmpr.dev <pmpr.dev@gmail.com>
2025-07-31 13:41:03 +0000

    Obfuscated

--- src/Pmpr/Custom/Filefox/Salary/ContentManager.php
+++ src/Pmpr/Custom/Filefox/Salary/ContentManager.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             688b675a6ec09             |
+    |             688b723de65fe             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Filefox\Salary; use Pmpr\Module\Salary\AbstractSalary; class ContentManager extends AbstractSalary { public function ikcgmcycisiccyuc() { $this->title = sprintf(__('FileFox Custom (%s)', PR__CST__FILEFOX), __('Content Manager', PR__CST__FILEFOX)); } public function ksikyqoayeggqssg($xssuewsokckmigqk, $cawesmkieccckaae, $product) : array { } public function vaakeoqesyogieoe($xssuewsokckmigqk, $cawesmkieccckaae, $product) : array { } }
--- src/Pmpr/Custom/Filefox/Ticket/Common.php
+++ src/Pmpr/Custom/Filefox/Ticket/Common.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             688b675a6ec09             |
+    |             688b723de65fe             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Filefox\Ticket; use Pmpr\Custom\Filefox\Container; abstract class Common extends Container { const gewoiiswcuiwggqo = 'file_request'; const suooagqkicoeawcy = 'file_provide'; }
