ea4f04e4469f74c15db5eb3c68cb9c63e4781963
pmpr.dev <pmpr.dev@gmail.com>
2025-08-14 23:21:20 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             689e6b7ddfd98             |
+    |             689e6f404cb23             |
     |_______________________________________|
 */
  use Pmpr\Custom\Filefox\Filefox; Filefox::symcgieuakksimmu();
--- src/Pmpr/Custom/Filefox/Container.php
+++ src/Pmpr/Custom/Filefox/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             689e6b7ddfd98             |
+    |             689e6f404cb23             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Filefox; use Pmpr\Common\Foundation\Container\Container as BaseClass; abstract class Container extends BaseClass { }
