756b00be9a3e2d51ecdbce538d3cf8a661dd04bc
pmpr.dev <pmpr.dev@gmail.com>
2025-08-14 22:37:13 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             689e5f2f913a5             |
+    |             689e64e6bccd7             |
     |_______________________________________|
 */
  use Pmpr\Custom\Filefox\Filefox; Filefox::symcgieuakksimmu();
--- src/Pmpr/Custom/Filefox/Panel/Controller/ContentManagement.php
+++ src/Pmpr/Custom/Filefox/Panel/Controller/ContentManagement.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             689e5f2f913a5             |
+    |             689e64e6bccd7             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Filefox\Panel\Controller; class ContentManagement extends Controller { public function __construct() { $this->rest_base = 'content-management'; parent::__construct(); } }
