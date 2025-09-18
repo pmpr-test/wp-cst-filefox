3e44d786d23cab70d4110fa449c25fda4906456d
pmpr.dev <pmpr.dev@gmail.com>
2025-09-18 21:37:54 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68cc71d73f6f0             |
+    |             68cc7b7e7f845             |
     |_______________________________________|
 */
  use Pmpr\Custom\Filefox\Filefox; Filefox::symcgieuakksimmu();
--- src/Pmpr/Custom/Filefox/Setting/SettingSegment.php
+++ src/Pmpr/Custom/Filefox/Setting/SettingSegment.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68cc71d73f6f0             |
+    |             68cc7b7e7f845             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Filefox\Setting; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Segment; class SettingSegment extends Segment { public function ikcgmcycisiccyuc() { $this->setting = Setting::symcgieuakksimmu(); } }
