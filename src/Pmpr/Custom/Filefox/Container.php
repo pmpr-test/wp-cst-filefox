38b038d5d22ae09d58884a040cc80cdd42d625db
pmpr.dev <pmpr.dev@gmail.com>
2025-09-29 20:27:21 +0000

    Obfuscated

--- src/Pmpr/Custom/Filefox/Container.php
+++ src/Pmpr/Custom/Filefox/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68dae6306d1b8             |
+    |             68daeb75c61fd             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Filefox; use Pmpr\Common\Foundation\Container\Container as BaseClass; abstract class Container extends BaseClass { }
--- src/Pmpr/Custom/Filefox/Setting/SettingSegment.php
+++ src/Pmpr/Custom/Filefox/Setting/SettingSegment.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68dae6306d1b8             |
+    |             68daeb75c61fd             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Filefox\Setting; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Segment; class SettingSegment extends Segment { public function ikcgmcycisiccyuc() { $this->setting = Setting::symcgieuakksimmu(); } }
