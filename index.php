c52b9e66b58b47a5e75ddeceb4e0590a48f43f5f
pmpr.dev <pmpr.dev@gmail.com>
2025-10-03 09:44:43 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             68dda3c87226b             |
+    |             68df9ad677bdb             |
     |_______________________________________|
 */
  use Pmpr\Custom\Filefox\Filefox; Filefox::symcgieuakksimmu();
